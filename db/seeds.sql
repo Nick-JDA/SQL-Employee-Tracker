f955ca05b7fd18bba2c4cd009e31be406726a547
Nick JDA <armstrongnick1999@gmail.com>
2023-06-06 19:41:07 -0400

    added js into questions js file such as .then with if statement to call functions for each operation, created viewAllDepartments, viewAllRoles, and viewAllEmployees functions to display information

--- db/seeds.sql
+++ db/seeds.sql
@@ -19,11 +19,11 @@ VALUES ("Employment manager", 4500.00, 1),
 
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Joey", "Yeoj", 1),
-("Jake", "Ekaj", 2, 11),
-("Jen", "Nej", 3),
-("Jack", "Kcaj", 4, 22),
-("Jeff", "Ffej", 5),
-("Jasmine", "Enimsaj", 6, 33),
-("Javier", "Reivaj", 7),
-("Jeorge", "Egroej", 8, 44);
+VALUES ("Joey", "Yeoj", 1, null),
+("Jake", "Ekaj", 2, 1),
+("Jen", "Nej", 3, null),
+("Jack", "Kcaj", 4, 2),
+("Jeff", "Ffej", 5, null),
+("Jasmine", "Enimsaj", 6, 3),
+("Javier", "Reivaj", 7, null),
+("Jeorge", "Egroej", 8, 4);
--- questions.js
+++ questions.js
@@ -1,21 +1,82 @@
 const inquirer = require('inquirer');
+const mysql = require('mysql2');
 
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // MySQL username,
+    user: 'root',
+    // TODO: Add MySQL password here
+    password: 'HuM4N4Ft3R4Ll!',
+    database: 'company_db'
+  },
+  console.log(`Connected to the company_db database.`)
+);
 
-inquirer
-      .prompt([
-        {
-          type: 'list',
-          name: 'response',
-          message: 'What would you like to do?',
-          choices: [
-            'View all departments',
-            'View all roles',
-            'View all employees',
-            'Add a department',
-            'Add a role',
-            'Add an employee',
-            'Update an employee role'
-          ]
-        },
-      ])
-      
+function askQuestion() {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'option',
+                message: 'What would you like to do?',
+                choices: [
+                    'View all departments',
+                    'View all roles',
+                    'View all employees',
+                    'Add a department',
+                    'Add a role',
+                    'Add an employee',
+                    'Update an employee role'
+                ]
+            },
+        ])
+        .then((response) => {
+            if (response.option == 'View all departments') {
+                viewAllDepartments(); 
+            } else if (response.option == 'View all roles') {
+                viewAllRoles();
+            } else if (response.option == 'View all employees') {
+                viewAllEmployees();
+            } else if (response == 'Add a department') {
+                addDepartment();
+            } else if (response == 'Add a role') {
+                addRole();
+            } else if (response == 'Add an employee') {
+                addEmployee();
+            } else if (response == 'Update an employee role') {
+                updateEmployeeRole();
+            }
+        })
+};
+askQuestion();
+
+//view all departments
+viewAllDepartments = () => {
+    db.query(`SELECT id as "Dept ID", dept_name as "Dept Name" FROM department`, (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        console.table(result);
+    });
+};
+
+//view all roles
+viewAllRoles = () => {
+    db.query(`SELECT r.id as "Role ID", r.title as "Job Title", r.salary, dept_name as Dept FROM role as r JOIN department ON r.department_id = department.id`, (err,result) => {
+        if (err) {
+            console.log(err);
+        }
+        console.table(result);
+    });
+};
+
+//view all employees
+viewAllEmployees = () => {
+    db.query(`SELECT * FROM employee`, (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        console.table(result);
+    });
+};
